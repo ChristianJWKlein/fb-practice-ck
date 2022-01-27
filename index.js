ed22de8cae7dc54da05a8e66db9285fad9c2060f
Christian John William Klein <cjwk@Christians-MacBook-Air.local>
2022-01-27 14:40:55 -0500

    draft

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const {
 
 // import our credentials
 const credentials = require("./credentials.json");
+const { forEach } = require("lodash");
 
 //connect to firebase services
 initializeApp({
@@ -24,14 +25,48 @@ initializeApp({
 //connect to firestore
 const db = getFirestore();
 
+// refernce to restaurant collect since used heavily
+const restRef = db.collection("restaurants");
+
 //create a collection called "restaurants"
 
 //add each restaurants
-db.collection("restaurants")
-  .add(restaurants[0])
-  .then((doc) => {
-    console.log("added restaurant", doc.id);
+// restRef
+//   .add(restaurants[1])
+//   .then((doc) => {
+//     console.log("added restaurant", doc.id);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+//read one document
+// db.collection(restaurants)
+//   .doc("rMPMYE1RhsOmmYQKIkcr")
+//   .get()
+//   .then((doc) => {
+//     console.log(doc.id, " => ", doc.data());
+//   })
+//   .catch((err) => console.error(err));
+
+//get all documents..below we actually get a snapshot of the database at that time.
+// restRef
+//   .get()
+//   .then((snapshot) =>
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, " => ", doc.data());
+//     })
+//   )
+//   .catch(console.error);
+
+//querying a collection
+restRef
+  .where("name", "==", "Bolay")
+  .get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      //todd may not have had snapshot on this line
+      console.log(doc.data());
+    });
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch(console.error);
