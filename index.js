fbd8c2b6126a53ec082d8d030c742e7bfbcf5b4a
Christian John William Klein <cjwk@Christians-MacBook-Air.local>
2022-01-27 10:59:23 -0500

    imported restaurants.json

---
+++ index.js
@@ -0,0 +1,9 @@
+//import our restaurants
+const restaurants = require("./restaurants.json");
+
+console.log(restaurants[0].name);
+//connect to firestore
+
+//create a collection called "restaurants"
+
+//add each restaurants
