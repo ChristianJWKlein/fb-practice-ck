d372e3877f1038238f96ca1aa4bc03b306e1a774
Christian John William Klein <cjwk@Christians-MacBook-Air.local>
2022-01-27 12:06:27 -0500

    checkpoint

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 //import our restaurants
 const restaurants = require("./restaurants.json");
 
-console.log(restaurants[0].name);
+//import a set of tools to talk to firebase and firestore
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore");
+
+// import our credentials
+const credentials = require("./credentials.json");
+
+//connect to firebase services
+initializeApp({
+  credential: cert(credentials),
+});
+
 //connect to firestore
+const db = getFirestore();
 
 //create a collection called "restaurants"
 
 //add each restaurants
+db.collection("restaurants")
+  .add(restaurants[0])
+  .then((doc) => {
+    console.log("added restaurant", doc.id);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
